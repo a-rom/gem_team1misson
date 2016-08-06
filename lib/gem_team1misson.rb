1f1da7c55ace42d955c1efa31262d2a4c72150e7
a-rom <a1623at@aiit.ac.jp>
2016-08-06 08:16:05 +0000

    modify gem_team1misson.rb

--- lib/gem_team1misson.rb
+++ lib/gem_team1misson.rb
@@ -1,24 +1,5 @@
 require "gem_team1misson/version"
-
-module GemTeam1misson
-#!/usr/bin/ruby
-class Main
-def bmi(n)
-puts'BMIを計算します'
-print'あなたの身長を入力してください'
-input_height=gets.chomp
-print'あなたの体重を入力してください'
-input_weight=gets.chomp
-bmi=input_weight.to_f/(input_height.to_f**2)
-
-if bmi<18.5
-  evaluation='SKINNY'
-    elsif bmi>=18.5 && bmi<25
-  evaluation='STANDARD'
-    elsif bmi>25
-  evaluation='FAT'
-end
-end
-
-end
-end
+    def bmi?(height, weight)         
+        s = weight / ( height / 100.0 ) ** 2  
+      return s 
+    end
