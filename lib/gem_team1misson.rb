c00d004e6762bf90ff8c76bcd1046ea9d8abcdde
Aritomo Moritoh <z14010am@aiit.ac.jp>
2016-08-06 06:56:32 +0000

    bmi test

--- lib/gem_team1misson.rb
+++ lib/gem_team1misson.rb
@@ -1,5 +1,23 @@
 require "gem_team1misson/version"
 
 module GemTeam1misson
-  # Your code goes here...
+#!/usr/bin/ruby
+
+puts'BMIを計算します'
+print'あなたの身長を入力してください'
+input_height=gets.chomp
+print'あなたの体重を入力してください'
+input_weight=gets.chomp
+bmi=input_weight.to_f/(input_height.to_f**2)
+
+if bmi<18.5
+  evaluation='SKINNY'
+    elsif bmi>=18.5 && bmi<25
+  evaluation='STANDARD'
+    elsif bmi>25
+  evaluation='FAT'
+
+
 end
+
+
